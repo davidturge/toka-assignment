c9c22ea22ec87e7a4b39004f23f7cf377298612c
davidturge <davidturge@gmail.com>
2024-11-27 15:11:18 +0200

    Add _mixins.scss, _variables.scss. Add Router with home, project placeholders Add Eslint

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Project from './pages/Project'
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/project/:id" element={<Project />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
